d885130f717a7c93a66c44ce30d095ed2831190f
samarddin <figmacom4@gmail.com>
2023-12-15 15:05:05 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { get } from "./reducers/todo";
+import { deleteUser } from "./reducers/todo";
+import "../src/App.css";
+import { setModal } from "./reducers/todo";
 import { useDispatch, useSelector } from "react-redux";
 function App() {
-  let dispatch = useDispatch()
-  let data = useSelector((store) => store.todo.data);
-  console.log(data);
+  let [inp , setInp] = useState("")
+  let dispatch = useDispatch();
+  let data = useSelector((store: any) => store.todo.data);
+  let modalEdit = useSelector((store: any) => store.todo.modalEdit);
+
   useEffect(() => {
-    dispatch(get())
-  },[])
-  return <div></div>;
+    dispatch(get());
+  }, []);
+  return (
+    <div id="divA">
+      {data.map((elem: any) => {
+        return (
+          <div id="div">
+            <img src={elem.img} alt="" />
+            <h1>{elem.name}</h1>
+            <div id="div2">
+              <button onClick={() => dispatch(setModal())}>Edit</button>
+              <button onClick={() => dispatch(deleteUser(elem.id))}>
+                Delete
+              </button>
+              <input type="checkbox" />
+            </div>
+          </div>
+        );
+      })}
+      {modalEdit? <div><input  value={inp} type="text" name="" id="" /> <button>Edit</button></div>:null}
+    </div>
+  );
 }
 
 export default App;
--- src/reducers/todo.ts
+++ src/reducers/todo.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 let api = "http://localhost:3000/data";
-export let get = createAsyncThunk("data/get", async function () {
+export let get: any = createAsyncThunk("data/get", async function () {
   try {
     let { data } = await axios.get(api);
     return data;
@@ -9,10 +9,26 @@ export let get = createAsyncThunk("data/get", async function () {
     console.error(error);
   }
 });
+export let deleteUser:any = createAsyncThunk(
+  "data/deleteUser",
+  async function (id, { dispatch }) {
+    try {
+      let { data } = await axios.delete(`${api}/${id}`);
+      dispatch(get());
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
 let todo = createSlice({
   name: "todo",
-  initialState: { data: [] },
-  reducers: {},
+  initialState: { data: [], modalEdit: false },
+  reducers: {
+    setModal: (state) => { 
+      state.modalEdit =  true
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(get.fulfilled, (state, action) => {
       state.data = action.payload;
@@ -20,3 +36,4 @@ let todo = createSlice({
   },
 });
 export default todo.reducer;
+export let { setModal } = todo.actions
